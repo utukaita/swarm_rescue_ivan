d1427a723f83c160a49e9e942fa7d73913b0d8b2
Emmanuel Battesti <emmanuel.battesti@ensta-paris.fr>
2024-11-08 18:05:00 +0100

    v4.0.2

--- src/swarm_rescue/solutions/my_drone_eval.py
+++ src/swarm_rescue/solutions/my_drone_eval.py
@@ -1,5 +1,443 @@
-from solutions.my_drone_random import MyDroneRandom
+import random
+import math
+from typing import Optional, List, Type
+from enum import Enum
+import numpy as np
+
+from spg_overlay.entities.drone_distance_sensors import DroneSemanticSensor
+from spg_overlay.utils.utils import normalize_angle, circular_mean
+
+import os
+import sys
+
+import cv2
+
+sys.path.insert(0,
+                os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from spg_overlay.utils.constants import MAX_RANGE_LIDAR_SENSOR
+from spg_overlay.utils.grid import Grid
+
+from spg_overlay.utils.pose import Pose
+from spg_overlay.entities.drone_abstract import DroneAbstract
 
 
-class MyDroneEval(MyDroneRandom):
-    pass
+class SemanticGrid(Grid):
+    def __init__(self,
+                 size_area_world,
+                 resolution: float,
+                 semantic_sensor): #semantic sensor
+        super().__init__(size_area_world=size_area_world,
+                         resolution=resolution)
+
+        self.size_area_world = size_area_world
+        self.resolution = resolution
+
+        self.semantic_sensor = semantic_sensor # Semantic sensor instead
+
+        self.x_max_grid: int = int(self.size_area_world[0] / self.resolution
+                                   + 0.5)
+        self.y_max_grid: int = int(self.size_area_world[1] / self.resolution
+                                   + 0.5)
+
+        self.grid = np.zeros((self.x_max_grid, self.y_max_grid))
+        self.zoomed_grid = np.empty((self.x_max_grid, self.y_max_grid))
+
+    def update_grid(self, pose: Pose):
+        """
+        Bayesian map update with new observation
+        lidar : lidar data
+        pose : corrected pose in world coordinates
+        """
+        EVERY_N = 3
+        LIDAR_DIST_CLIP = 40.0
+        EMPTY_ZONE_VALUE = -0.602
+        OBSTACLE_ZONE_VALUE = 2.0
+        FREE_ZONE_VALUE = -4.0
+        THRESHOLD_MIN = -40
+        THRESHOLD_MAX = 40
+
+        lidar_dist = self.lidar.get_sensor_values()[::EVERY_N].copy()
+        lidar_angles = self.lidar.ray_angles[::EVERY_N].copy()
+
+        # Compute cos and sin of the absolute angle of the lidar
+        cos_rays = np.cos(lidar_angles + pose.orientation)
+        sin_rays = np.sin(lidar_angles + pose.orientation)
+
+        max_range = MAX_RANGE_LIDAR_SENSOR * 0.9
+
+        # For empty zones
+        # points_x and point_y contains the border of detected empty zone
+        # We use a value a little bit less than LIDAR_DIST_CLIP because of the
+        # noise in lidar
+        lidar_dist_empty = np.maximum(lidar_dist - LIDAR_DIST_CLIP, 0.0)
+        # All values of lidar_dist_empty_clip are now <= max_range
+        lidar_dist_empty_clip = np.minimum(lidar_dist_empty, max_range)
+        points_x = pose.position[0] + np.multiply(lidar_dist_empty_clip,
+                                                  cos_rays)
+        points_y = pose.position[1] + np.multiply(lidar_dist_empty_clip,
+                                                  sin_rays)
+
+        for pt_x, pt_y in zip(points_x, points_y):
+            self.add_value_along_line(pose.position[0], pose.position[1],
+                                      pt_x, pt_y,
+                                      EMPTY_ZONE_VALUE)
+
+        # For obstacle zones, all values of lidar_dist are < max_range
+        select_collision = lidar_dist < max_range
+
+        points_x = pose.position[0] + np.multiply(lidar_dist, cos_rays)
+        points_y = pose.position[1] + np.multiply(lidar_dist, sin_rays)
+
+        points_x = points_x[select_collision]
+        points_y = points_y[select_collision]
+
+        self.add_points(points_x, points_y, OBSTACLE_ZONE_VALUE)
+
+        # the current position of the drone is free !
+        self.add_points(pose.position[0], pose.position[1], FREE_ZONE_VALUE)
+
+        # threshold values
+        self.grid = np.clip(self.grid, THRESHOLD_MIN, THRESHOLD_MAX)
+
+        # compute zoomed grid for displaying
+        self.zoomed_grid = self.grid.copy()
+        new_zoomed_size = (int(self.size_area_world[1] * 0.5),
+                           int(self.size_area_world[0] * 0.5))
+        self.zoomed_grid = cv2.resize(self.zoomed_grid, new_zoomed_size,
+                                      interpolation=cv2.INTER_NEAREST)
+
+
+class OccupancyGrid(Grid):
+    """Simple occupancy grid"""
+
+    def __init__(self,
+                 size_area_world,
+                 resolution: float,
+                 lidar):
+        super().__init__(size_area_world=size_area_world,
+                         resolution=resolution)
+
+        self.size_area_world = size_area_world
+        self.resolution = resolution
+
+        self.lidar = lidar
+
+        self.x_max_grid: int = int(self.size_area_world[0] / self.resolution
+                                   + 0.5)
+        self.y_max_grid: int = int(self.size_area_world[1] / self.resolution
+                                   + 0.5)
+
+        self.grid = np.zeros((self.x_max_grid, self.y_max_grid))
+        self.zoomed_grid = np.empty((self.x_max_grid, self.y_max_grid))
+
+    def update_grid(self, pose: Pose):
+        """
+        Bayesian map update with new observation
+        lidar : lidar data
+        pose : corrected pose in world coordinates
+        """
+        EVERY_N = 3
+        LIDAR_DIST_CLIP = 40.0
+        EMPTY_ZONE_VALUE = -0.602
+        OBSTACLE_ZONE_VALUE = 2.0
+        FREE_ZONE_VALUE = -4.0
+        THRESHOLD_MIN = -40
+        THRESHOLD_MAX = 40
+
+        lidar_dist = self.lidar.get_sensor_values()[::EVERY_N].copy()
+        lidar_angles = self.lidar.ray_angles[::EVERY_N].copy()
+
+        # Compute cos and sin of the absolute angle of the lidar
+        cos_rays = np.cos(lidar_angles + pose.orientation)
+        sin_rays = np.sin(lidar_angles + pose.orientation)
+
+        max_range = MAX_RANGE_LIDAR_SENSOR * 0.9
+
+        # For empty zones
+        # points_x and point_y contains the border of detected empty zone
+        # We use a value a little bit less than LIDAR_DIST_CLIP because of the
+        # noise in lidar
+        lidar_dist_empty = np.maximum(lidar_dist - LIDAR_DIST_CLIP, 0.0)
+        # All values of lidar_dist_empty_clip are now <= max_range
+        lidar_dist_empty_clip = np.minimum(lidar_dist_empty, max_range)
+        points_x = pose.position[0] + np.multiply(lidar_dist_empty_clip,
+                                                  cos_rays)
+        points_y = pose.position[1] + np.multiply(lidar_dist_empty_clip,
+                                                  sin_rays)
+
+        for pt_x, pt_y in zip(points_x, points_y):
+            self.add_value_along_line(pose.position[0], pose.position[1],
+                                      pt_x, pt_y,
+                                      EMPTY_ZONE_VALUE)
+
+        # For obstacle zones, all values of lidar_dist are < max_range
+        select_collision = lidar_dist < max_range
+
+        points_x = pose.position[0] + np.multiply(lidar_dist, cos_rays)
+        points_y = pose.position[1] + np.multiply(lidar_dist, sin_rays)
+
+        points_x = points_x[select_collision]
+        points_y = points_y[select_collision]
+
+        self.add_points(points_x, points_y, OBSTACLE_ZONE_VALUE)
+
+        # the current position of the drone is free !
+        self.add_points(pose.position[0], pose.position[1], FREE_ZONE_VALUE)
+
+        # threshold values
+        self.grid = np.clip(self.grid, THRESHOLD_MIN, THRESHOLD_MAX)
+
+        # compute zoomed grid for displaying
+        self.zoomed_grid = self.grid.copy()
+        new_zoomed_size = (int(self.size_area_world[1] * 0.5),
+                           int(self.size_area_world[0] * 0.5))
+        self.zoomed_grid = cv2.resize(self.zoomed_grid, new_zoomed_size,
+                                      interpolation=cv2.INTER_NEAREST)
+
+
+class MyDroneEval(DroneAbstract):
+    class Activity(Enum):
+        """
+        All the states of the drone as a state machine
+        """
+        EXPLORING = 0
+        SEARCHING_WOUNDED = 1
+        GRASPING_WOUNDED = 2
+        SEARCHING_RESCUE_CENTER = 3
+        DROPPING_AT_RESCUE_CENTER = 4
+
+    def __init__(self,
+                 identifier: Optional[int] = None, **kwargs):
+        super().__init__(identifier=identifier,
+                         display_lidar_graph=False,
+                         **kwargs)
+        # The state is initialized to searching wounded person
+        self.state = self.Activity.SEARCHING_WOUNDED
+
+        # Those values are used by the random control function
+        self.counterStraight = 0
+        self.angleStopTurning = 0
+        self.isTurning = False
+
+        self.iteration: int = 0
+
+        self.estimated_pose = Pose()
+
+        resolution = 8
+        self.grid = OccupancyGrid(size_area_world=self.size_area,
+                                  resolution=resolution,
+                                  lidar=self.lidar())
+
+    def define_message_for_all(self):
+        """
+        Sharing the map
+        Sharing positions of wounded people
+        Sharing if carrying a wounded person
+
+        Test different messaging periods
+        """
+        pass
+
+    def control(self):
+        command = {"forward": 0.0,
+                   "lateral": 0.0,
+                   "rotation": 0.0,
+                   "grasper": 0}
+
+        found_wounded, found_rescue_center, command_semantic = (
+            self.process_semantic_sensor())
+
+        #############
+        # TRANSITIONS OF THE STATE MACHINE
+        #############
+
+        if self.state is self.Activity.EXPLORING or self.Activity.SEARCHING_WOUNDED and found_wounded:
+            self.state = self.Activity.GRASPING_WOUNDED
+
+        elif (self.state is self.Activity.GRASPING_WOUNDED and
+              self.base.grasper.grasped_entities):
+            self.state = self.Activity.SEARCHING_RESCUE_CENTER
+
+        elif (self.state is self.Activity.GRASPING_WOUNDED and
+              not found_wounded):
+            self.state = self.Activity.SEARCHING_WOUNDED
+
+        elif (self.state is self.Activity.SEARCHING_RESCUE_CENTER and
+              found_rescue_center):
+            self.state = self.Activity.DROPPING_AT_RESCUE_CENTER
+
+        elif (self.state is self.Activity.DROPPING_AT_RESCUE_CENTER and
+              not self.base.grasper.grasped_entities):
+            self.state = self.Activity.EXPLORING
+
+        elif (self.state is self.Activity.DROPPING_AT_RESCUE_CENTER and
+              not found_rescue_center):
+            self.state = self.Activity.SEARCHING_RESCUE_CENTER
+
+        print("state: {}, can_grasp: {}, grasped entities: {}"
+              .format(self.state.name,
+                      self.base.grasper.can_grasp,
+                      self.base.grasper.grasped_entities))
+
+        ##########
+        # COMMANDS FOR EACH STATE
+        # Searching randomly, but when a rescue center or wounded person is
+        # detected, we use a special command
+        ##########
+        if self.state is self.Activity.EXPLORING:
+            command = self.control_random()
+            command["grasper"] = 0
+
+        if self.state is self.Activity.SEARCHING_WOUNDED:
+            command = self.control_random()
+            command["grasper"] = 0
+
+        elif self.state is self.Activity.GRASPING_WOUNDED:
+            command = command_semantic
+            command["grasper"] = 1
+
+        elif self.state is self.Activity.SEARCHING_RESCUE_CENTER:
+            command = self.control_random()
+            command["grasper"] = 1
+
+        elif self.state is self.Activity.DROPPING_AT_RESCUE_CENTER:
+            command = command_semantic
+            command["grasper"] = 1
+
+        # increment the iteration counter
+        self.iteration += 1
+
+        self.estimated_pose = Pose(np.asarray(self.measured_gps_position()),
+                                       self.measured_compass_angle())
+        # self.estimated_pose = Pose(np.asarray(self.true_position()),
+        #                            self.true_angle())
+
+        self.grid.update_grid(pose=self.estimated_pose)
+        if self.iteration % 5 == 0:
+            self.grid.display(self.grid.grid,
+                                  self.estimated_pose,
+                                  title="occupancy grid")
+            self.grid.display(self.grid.zoomed_grid,
+                                  self.estimated_pose,
+                                  title="zoomed occupancy grid")
+                # pass
+        # Update semantics on the map
+
+        return command
+
+    def process_lidar_sensor(self):
+        """
+        Returns True if the drone collided an obstacle
+        """
+        if self.lidar_values() is None:
+            return False
+
+        collided = False
+        dist = min(self.lidar_values())
+
+        if dist < 40:
+            collided = True
+
+        return collided
+
+    def control_random(self):
+        """
+        The Drone will move forward and turn for a random angle when an
+        obstacle is hit
+        """
+        command_straight = {"forward": 0.5,
+                            "rotation": 0.0}
+
+        command_turn = {"forward": 0.0,
+                        "rotation": 1.0}
+
+        collided = self.process_lidar_sensor()
+
+        self.counterStraight += 1
+
+        if collided and not self.isTurning and self.counterStraight > 100:
+            self.isTurning = True
+            self.angleStopTurning = random.uniform(-math.pi, math.pi)
+
+        diff_angle = normalize_angle(
+            self.angleStopTurning - self.measured_compass_angle())
+        if self.isTurning and abs(diff_angle) < 0.2:
+            self.isTurning = False
+            self.counterStraight = 0
+
+        if self.isTurning:
+            return command_turn
+        else:
+            return command_straight
+
+    def process_semantic_sensor(self):
+        """
+        According to his state in the state machine, the Drone will move
+        towards a wound person or the rescue center
+        """
+        command = {"forward": 0.5,
+                   "lateral": 0.0,
+                   "rotation": 0.0}
+        angular_vel_controller_max = 1.0
+
+        detection_semantic = self.semantic_values()
+        best_angle = 0
+
+        found_wounded = False
+        if (self.state is self.EXPLORING
+            or self.state is self.Activity.SEARCHING_WOUNDED
+            or self.state is self.Activity.GRASPING_WOUNDED) \
+                and detection_semantic is not None:
+            scores = []
+            for data in detection_semantic:
+                # If the wounded person detected is held by nobody
+                if (data.entity_type ==
+                        DroneSemanticSensor.TypeEntity.WOUNDED_PERSON and
+                        not data.grasped): # And not grasped by another drone
+                    found_wounded = True
+                    v = (data.angle * data.angle) + \
+                        (data.distance * data.distance / 10 ** 5)
+                    scores.append((v, data.angle, data.distance))
+
+            # Select the best one among wounded persons detected
+            best_score = 10000
+            for score in scores:
+                if score[0] < best_score:
+                    best_score = score[0]
+                    best_angle = score[1]
+
+        found_rescue_center = False
+        is_near = False
+        angles_list = []
+        if (self.state is self.Activity.SEARCHING_RESCUE_CENTER
+            or self.state is self.Activity.DROPPING_AT_RESCUE_CENTER) \
+                and detection_semantic:
+            for data in detection_semantic:
+                if (data.entity_type ==
+                        DroneSemanticSensor.TypeEntity.RESCUE_CENTER):
+                    found_rescue_center = True
+                    angles_list.append(data.angle)
+                    is_near = (data.distance < 30)
+
+            if found_rescue_center:
+                best_angle = circular_mean(np.array(angles_list))
+
+        if found_rescue_center or found_wounded:
+            # simple P controller
+            # The robot will turn until best_angle is 0
+            kp = 2.0
+            a = kp * best_angle
+            a = min(a, 1.0)
+            a = max(a, -1.0)
+            command["rotation"] = a * angular_vel_controller_max
+
+            # reduce speed if we need to turn a lot
+            if abs(a) == 1:
+                command["forward"] = 0.2
+
+        if found_rescue_center and is_near:
+            command["forward"] = 0.0
+            command["rotation"] = -1.0
+
+        return found_wounded, found_rescue_center, command
